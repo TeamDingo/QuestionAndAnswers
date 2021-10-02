5004ce1988751193aac408f0fedf34b725c31ae5
bkern98 <bkern288@gmail.com>
2021-10-02 10:02:38 -0500

    Fixed error handling

--- db/index.js
+++ db/index.js
@@ -9,98 +9,70 @@ pool
   .then(() => console.log('Connected to database'))
   .catch(() => console.log('Error connecting to database'));
 
-const getQuestions = async productId => {
-  try {
-    const questions = await pool.query(
-      'SELECT * FROM questions WHERE product_id = $1',
-      [productId]
-    );
-    return questions.rows;
-  } catch (err) {
-    return err.stack;
-  }
+const getQuestions = productId => {
+  return pool.query('SELECT * FROM questions WHERE product_id = $1', [
+    productId
+  ]);
 };
 
-const getAnswers = async questionId => {
-  try {
-    const answers = await pool.query(
-      'SELECT a.answer_id, a.question_id, a.body,a.answerer_name, a.helpfulness, a.date, ARRAY_AGG (p.url) photos FROM answers a LEFT OUTER JOIN photos p ON a.answer_id = p.answer_id WHERE question_id = $1 GROUP BY a.answer_id',
-      [questionId]
-    );
-    return answers.rows;
-  } catch (err) {
-    return err.stack;
-  }
+const getAnswers = questionId => {
+  return pool.query(
+    'SELECT a.answer_id, a.question_id, a.body,a.answerer_name, a.helpfulness, a.date, ARRAY_AGG (p.url) photos FROM answers a LEFT OUTER JOIN photos p ON a.answer_id = p.answer_id WHERE question_id = $1 GROUP BY a.answer_id',
+    [questionId]
+  );
 };
 
-const writeQuestion = async (productId, body, name, email) => {
-  try {
-    const newQuestion = await pool.query(
-      'INSERT INTO questions (product_id, question_body, asker_name, asker_email) VALUES ($1, $2, $3, $4) RETURNING *',
-      [productId, body, name, email]
-    );
-    return newQuestion.rows[0];
-  } catch (err) {
-    return err.stack;
-  }
+const writeQuestion = (productId, body, name, email) => {
+  return pool.query(
+    'INSERT INTO questions (product_id, question_body, asker_name, asker_email) VALUES ($1, $2, $3, $4) RETURNING *',
+    [productId, body, name, email]
+  );
 };
 const writeAnswer = async (questionId, body, name, email, photos) => {
-  try {
-    const newAnswer = await pool.query(
-      'INSERT INTO answers (question_id, body, answerer_name, answerer_email) VALUES ($1, $2, $3, $4) RETURNING *',
-      [questionId, body, name, email]
-    );
-    const newPhotos = [];
-    if (photos) {
-      for (const photo of photos) {
-        const newPhoto = await pool.query(
-          'INSERT INTO photos (answer_id, url) VALUES ($1, $2) RETURNING *',
-          [newAnswer.rows[0].id, photo]
-        );
-        newPhotos.push(newPhoto.rows[0]);
-      }
+  const newAnswer = await pool.query(
+    'INSERT INTO answers (question_id, body, answerer_name, answerer_email) VALUES ($1, $2, $3, $4) RETURNING *',
+    [questionId, body, name, email]
+  );
+  const newPhotos = [];
+  if (photos) {
+    for (const photo of photos) {
+      const newPhoto = await pool.query(
+        'INSERT INTO photos (answer_id, url) VALUES ($1, $2) RETURNING *',
+        [newAnswer.rows[0].answer_id, photo]
+      );
+      newPhotos.push(newPhoto.rows[0]);
     }
-    return { answer: newAnswer.rows[0], photos: newPhotos };
-  } catch (err) {
-    return err.stack;
   }
+  return { ...newAnswer.rows[0], photos: newPhotos };
 };
 
-const markQuestionHelpful = async (questionId) => {
-  try {
-    const updatedQuestion = await pool.query('UPDATE questions SET question_helpfulness = question_helpfulness + 1 WHERE question_id = $1 RETURNING *', [questionId]);
-    return updatedQuestion.rows[0];
-  } catch (err) {
-    return err.stack;
-  }
-}
+const markQuestionHelpful = questionId => {
+  return pool.query(
+    'UPDATE questions SET question_helpfulness = question_helpfulness + 1 WHERE question_id = $1 RETURNING *',
+    [questionId]
+  );
+};
 
-const reportQuestion = async (questionId) => {
-  try {
-    const reportedQuestion = await pool.query('UPDATE questions SET reported = NOT reported WHERE question_id = $1 RETURNING *', [questionId]);
-    return reportedQuestion.rows[0];
-  } catch (err) {
-    return err.stack;
-  }
-}
+const reportQuestion = questionId => {
+  return pool.query(
+    'UPDATE questions SET reported = NOT reported WHERE question_id = $1 RETURNING *',
+    [questionId]
+  );
+};
 
-const markAnswerHelpful = async (answerId) => {
-  try {
-    const updatedAnswer = await pool.query('UPDATE answers SET helpfulness = helpfulness + 1 WHERE answer_id = $1 RETURNING *', [answerId]);
-    return updatedAnswer.rows[0];
-  } catch (err) {
-    return err.stack;
-  }
-}
+const markAnswerHelpful = answerId => {
+  return pool.query(
+    'UPDATE answers SET helpfulness = helpfulness + 1 WHERE answer_id = $1 RETURNING *',
+    [answerId]
+  );
+};
 
-const reportAnswer = async (answerId) => {
-  try {
-    const reportedAnswer = await pool.query('UPDATE answers SET reported = NOT reported WHERE answer_id = $1 RETURNING *', [answerId]);
-    return reportedAnswer.rows[0];
-  } catch (err) {
-    return err.stack;
-  }
-}
+const reportAnswer = answerId => {
+  return pool.query(
+    'UPDATE answers SET reported = NOT reported WHERE answer_id = $1 RETURNING *',
+    [answerId]
+  );
+};
 
 module.exports = {
   getQuestions,
--- server/index.js
+++ server/index.js
@@ -12,48 +12,98 @@ app.use(express.urlencoded({ extended: true }));
 
 // Retrieves all questions for a product
 app.get('/qa/:productId', async (req, res) => {
-  const questionsArray = await db.getQuestions(req.params.productId);
-  res.send(questionsArray);
+  try {
+    const questions = await db.getQuestions(req.params.productId);
+    res.send(questions.rows);
+  } catch (err) {
+    console.log(err.stack);
+    res.status(404).send();
+  }
 });
 
 // Retrieves all answers for a question
 app.get('/qa/:questionId/answers', async (req, res) => {
-  const answersArray = await db.getAnswers(req.params.questionId);
-  res.send(answersArray);
+  try {
+    const answers = await db.getAnswers(req.params.questionId);
+    res.send(answers.rows);
+  } catch (err) {
+    console.log(err.stack);
+    res.status(404).send();
+  }
 });
 
 // Write a question
 app.post('/qa/:productId', async (req, res) => {
-  const newQuestionData = await db.writeQuestion(req.params.productId, req.body.body, req.body.name, req.body.email);
-  res.send(newQuestionData);
+  try {
+    const newQuestion = await db.writeQuestion(
+      req.params.productId,
+      req.body.body,
+      req.body.name,
+      req.body.email
+    );
+    res.send(newQuestion.rows[0]);
+  } catch (err) {
+    console.log(err.stack);
+    res.status(400).send();
+  }
 });
 
 // Write an answer
 app.post('/qa/:questionId/answers', async (req, res) => {
-  const newAnswerData = await db.writeAnswer(req.params.questionId, req.body.body, req.body.name, req.body.email, req.body.photos);
-  res.send(newAnswerData);
+  try {
+    const newAnswerData = await db.writeAnswer(
+      req.params.questionId,
+      req.body.body,
+      req.body.name,
+      req.body.email,
+      req.body.photos
+    );
+    res.send(newAnswerData);
+  } catch (err) {
+    console.log(err.stack);
+    res.status(400).send();
+  }
 });
 
 app.put('/qa/question/:questionId/helpful', async (req, res) => {
-  const updatedQuestionData = await db.markQuestionHelpful(req.params.questionId);
-  res.send(updatedQuestionData);
+  try {
+    const updatedQuestion = await db.markQuestionHelpful(req.params.questionId);
+    res.send(updatedQuestion.rows[0]);
+  } catch (err) {
+    console.log(err.stack);
+    res.status(400).send();
+  }
 });
 
 app.put('/qa/question/:questionId/report', async (req, res) => {
-  const reportedQuestionData = await db.reportQuestion(req.params.questionId);
-  res.send(reportedQuestionData);
+  try {
+    const reportedQuestion = await db.reportQuestion(req.params.questionId);
+    res.send(reportedQuestion.rows[0]);
+  } catch (err) {
+    console.log(err.stack);
+    res.status(400).send();
+  }
 });
 
 app.put('/qa/answer/:answerId/helpful', async (req, res) => {
-  const updatedAnswerData = await db.markAnswerHelpful(req.params.answerId);
-  res.send(updatedAnswerData);
+  try {
+    const updatedAnswer = await db.markAnswerHelpful(req.params.answerId);
+    res.send(updatedAnswer.rows[0]);
+  } catch (err) {
+    console.log(err.stack);
+    res.status(400).send();
+  }
 });
 
 app.put('/qa/answer/:answerId/report', async (req, res) => {
-  const reportedAnswerData = await db.reportAnswer(req.params.answerId);
-  res.send(reportedAnswerData);
+  try {
+    const reportedAnswer = await db.reportAnswer(req.params.answerId);
+    res.send(reportedAnswer.rows[0]);
+  } catch (err) {
+    console.log(err.stack);
+    res.status(400).send();
+  }
 });
-
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
